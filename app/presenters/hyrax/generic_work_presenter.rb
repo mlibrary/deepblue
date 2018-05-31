bb41dc85f0cdd639147ac11b9ed8f3cfda9ec8b9
Blanco <blancoj@umich.edu>
2018-05-31 14:53:35 -0400

    for show page of generic work

--- app/presenters/hyrax/generic_work_presenter.rb
+++ app/presenters/hyrax/generic_work_presenter.rb
@@ -2,5 +2,9 @@
 #  `rails generate hyrax:work GenericWork`
 module Hyrax
   class GenericWorkPresenter < Hyrax::WorkShowPresenter
+
+    delegate  :identifier_orcid, :academic_affiliation, :other_affiliation, :contributor_affiliationumcampus, :alt_title, :date_issued, :identifier_source,  :peerreviewed, :bibliographic_citation, :relation_ispartofseries,  :rights_statement, :type_none, :language_none, :description_mapping,  :description_abstract, :description_sponsorship, :description, to: :solr_document 
+
+
   end
 end
