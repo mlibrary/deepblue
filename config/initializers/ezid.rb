bb5d704514425e86f7c79b71983ebdbe99fc97ef
Noah Botimer <botimer@umich.edu>
2019-03-09 16:03:33 -0500

    Normalize configurations to use Settings
    There are many small changes here to normalize the configuration such
    that it can be committed and have convenient overrides where needed. The
    pattern of using .sample files meant that every environment must
    generate copies, which leads to needless duplication and desynchronization,
    making it difficult to manage multiple instances and changes over time.
    
    The config gem in use allows for local overrides of specific settings,
    rather than entire files. This means that generic files that refer to
    Settings can be committed and an instance may customize only what it
    needs; for example, the Solr core name.
    
    There is also some reorganization and renaming of the settings and how
    they are applied within the application, to improve consistency. For
    example, the settings that are applied in the Hyrax initializer are
    nested under a hyrax key. There are some notes of where further work
    should be done.
    
    Where possible, the YAML files use aliases to reduce the duplication of
    key expressions. This makes files like fedora.yml very generic, but
    there are comments that specific values should not be placed there and
    references to the other files where they should be placed. The master
    settings.yml file is commented to describe the sections and the settings
    within them.
    
    Re: AEIM-1906

--- config/initializers/ezid.rb
+++ config/initializers/ezid.rb
@@ -1,42 +1,21 @@
 # frozen_string_literal: true
 
-require 'erb'
-require 'yaml'
-
-# Yeah, there's many better ways to do this, but later.
-def load_yml
-  cfg_file = "#{::Rails.root}/config/ezid.yml"
-
-  begin
-    ezid_erb = ERB.new(IO.read(cfg_file)).result(binding)
-  rescue StandardError, SyntaxError => e
-    raise("#{cfg_file} could not be parsed with ERB. \n#{e.inspect}")
-  end
-
-  begin
-    ezid_yml = YAML.safe_load(ezid_erb)
-  rescue => e
-    raise("#{cfg_file} was found, but could not be parsed.\n#{e.inspect}")
-  end
-
-  if ezid_yml.nil? || !ezid_yml.is_a?(Hash)
-    raise("#{cfg_file} was found, but was blank or malformed.\n")
-  end
-
-  begin
-    raise "The #{::Rails.env} environment settings were not found in #{cfg_file}" unless ezid_yml[::Rails.env]
-    ezid_cfg = ezid_yml[::Rails.env].symbolize_keys
-  end
-
-  ezid_cfg
-end
-
 Ezid::Client.configure do |config|
-  yml_cfg = load_yml
-  config.host = yml_cfg[:host] || "localhost"
-  config.port = yml_cfg[:port] || 8443
-  # config.use_ssl  = yml_cfg[:use_ssl] || true
-  config.user     = yml_cfg[:user] || "eziduser"
-  config.password = yml_cfg[:password] || "ezidpass"
-  config.default_shoulder = yml_cfg[:shoulder] || "ark:/99999/"
+  # Decide whether to use SSL -- default to true
+  # ENV always applies if present, must be "false" to disable
+  # If ENV is not present, Settings.use_ssl must be false or "false" to disable
+  env = ENV['EZID_USE_SSL']
+  use_ssl = if env
+              env.to_s != 'false'
+            else
+              Settings.ezid.use_ssl.to_s != 'false'
+            end
+
+  config.host     = ENV['EZID_HOST'] || Settings.ezid.host
+  config.port     = ENV['EZID_PORT'] || Settings.ezid.port
+  config.use_ssl  = use_ssl
+  config.user     = ENV['EZID_USER'] || Settings.ezid.user
+  config.password = ENV['EZID_PASSWORD'] || Settings.ezid.password
+  config.timeout  = ENV['EZID_TIMEOUT'] || Settings.ezid.timeout
+  config.default_shoulder = ENV['EZID_DEFAULT_SHOULDER'] || Settings.ezid.shoulder
 end
