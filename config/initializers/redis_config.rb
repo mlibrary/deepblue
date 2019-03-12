818d3a2d248281b542fcf57d5b3ba4ce464817d8
Fritz Freiheit <fritx@umich.edu>
2019-03-12 11:49:49 -0400

    Revert "Normalize configurations to use Settings"

--- config/initializers/redis_config.rb
+++ config/initializers/redis_config.rb
@@ -1,2 +1,3 @@
 require 'redis'
-Redis.current = Redis.new(Settings.redis.to_h)
+config = YAML.safe_load(ERB.new(IO.read(Rails.root.join('config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
+Redis.current = Redis.new(config.merge(thread_safe: true))
