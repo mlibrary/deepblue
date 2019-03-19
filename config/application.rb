c62cf5e0d98b3e5cb9f57ec4a19f9fc883d48032
Noah Botimer <botimer@umich.edu>
2019-03-18 20:20:38 -0400

    Ignore the relative URL root in test; this is bad!
    This change fixes breaking tests at the cost of ignoring a preexisting
    problem. When normalizing the config, I removed the conditionality here
    to see what might break when this assumption no longer held.
    
    Basically, the application must run at /data because there are various
    places that hard-code the prefix, but the tests must not run as though
    anywhere but /. This means that the relative_url_root is not handled
    consistently for link generation, which should be thoroughly audited.
    
    The tests that failed without the "unless" are not the entire scope.
    There are places where the prefix is added manually, and places where it
    is assumed not to be present, many of which are invisible to the test
    suite as it stands. Getting the tests to work on an arbitrary
    relative_url_root would eliminate this "unless", but not solve the
    larger issue, which may be tolerable as long as the app will only ever
    run under /data URLs.

--- config/application.rb
+++ config/application.rb
@@ -125,7 +125,9 @@ module DeepBlueDocs
     # URL for logging the user out of Cosign
     config.logout_prefix = "https://weblogin.umich.edu/cgi-bin/logout?"
 
-    config.relative_url_root = Settings.relative_url_root
+    # FIXME: This unless reveals bugs. There are places in the app that hard-code the
+    #        /data prefix and the tests break when it is set.
+    config.relative_url_root = Settings.relative_url_root unless Rails.env.test?
 
     # For properly generating URLs and minting DOIs - the app may not by default
     # Outside of a request context the hostname needs to be provided.
