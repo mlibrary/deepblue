683a55141f9a7be9018171b4734df48cc05f6a0e
Fritz Freiheit <fritx@umich.edu>
2019-03-26 16:08:01 -0400

    BLUEDOC-465 - Enforce timezone to current for date displays
    * Clean up dates on file version list
    * Added option for version dates to be local timezone or UTC
    * Clean up dates on activity log and option to be local or UTC

--- app/helpers/deepblue_helper.rb
+++ app/helpers/deepblue_helper.rb
@@ -2,7 +2,18 @@
 
 module DeepblueHelper
 
-  def self.human_readable_size( value, precision: 3 )
+	def self.display_timestamp( timestamp )
+		timestamp = timestamp.to_datetime if timestamp.is_a? Time
+		timestamp = DateTime.parse timestamp if timestamp.is_a? String
+		if DeepBlueDocs::Application.config.datetime_stamp_display_local_time_zone
+			timestamp = timestamp.new_offset( DeepBlueDocs::Application.config.timezone_offset )
+			"#{timestamp.strftime("%Y-%m-%d %H:%M:%S")}"
+		else
+			"#{timestamp.strftime("%Y-%m-%d %H:%M:%S")} #{timestamp.formatted_offset(false, 'UTC')}"
+		end
+	end
+
+	def self.human_readable_size( value, precision: 3 )
     ActiveSupport::NumberHelper::NumberToHumanSizeConverter.convert( value, precision: precision )
   end
 
@@ -17,41 +28,41 @@ module DeepblueHelper
 		  if user_agent.index('msie') && !user_agent.index('opera') && !user_agent.index('webtv')
 		                # 'ie'+user_agent[user_agent.index('msie')+5].chr
 		                'msie'
-		    elsif user_agent.index('gecko/')
-		        'gecko'
-		    elsif user_agent.index('opera')
-		        'opera'
-		    elsif user_agent.index('konqueror')
-		        'konqueror'
-		    elsif user_agent.index('ipod')
-		        'ipod'
-		    elsif user_agent.index('ipad')
-		        'ipad'
-		    elsif user_agent.index('iphone')
-		        'iphone'
-		    elsif user_agent.index('chrome/')
-		        'chrome'
-		    elsif user_agent.index('applewebkit/')
-		        'safari'
-		    elsif user_agent.index('googlebot/')
-		        'googlebot'
-		    elsif user_agent.index('msnbot')
-		        'msnbot'
-		    elsif user_agent.index('yahoo! slurp')
-		        'yahoobot'
-		    elsif user_agent.index('mozilla/5.0 (windows nt 6.3; win64, x64')
-		        'msie'
-		    elsif user_agent.index('mozilla/5.0 (windows nt 10.0; win64; x64)')
-		        'msie'  
-		    #Everything thinks it's mozilla, so this goes last
-		    elsif user_agent.index('mozilla/')
-		        'gecko'
-		    else
-		        'unknown'
-		    end
-		    end
+      elsif user_agent.index('gecko/')
+          'gecko'
+      elsif user_agent.index('opera')
+          'opera'
+      elsif user_agent.index('konqueror')
+          'konqueror'
+      elsif user_agent.index('ipod')
+          'ipod'
+      elsif user_agent.index('ipad')
+          'ipad'
+      elsif user_agent.index('iphone')
+          'iphone'
+      elsif user_agent.index('chrome/')
+          'chrome'
+      elsif user_agent.index('applewebkit/')
+          'safari'
+      elsif user_agent.index('googlebot/')
+          'googlebot'
+      elsif user_agent.index('msnbot')
+          'msnbot'
+      elsif user_agent.index('yahoo! slurp')
+          'yahoobot'
+      elsif user_agent.index('mozilla/5.0 (windows nt 6.3; win64, x64')
+          'msie'
+      elsif user_agent.index('mozilla/5.0 (windows nt 10.0; win64; x64)')
+          'msie'
+      #Everything thinks it's mozilla, so this goes last
+      elsif user_agent.index('mozilla/')
+          'gecko'
+      else
+          'unknown'
+      end
+    end
 
-		    return @users_browser
+    return @users_browser
 	end
 
 end
