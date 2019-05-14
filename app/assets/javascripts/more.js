e27c36b236126f0c95cfcbe8d36260aaa5de270f
Blanco <blancoj@umich.edu>
2019-05-14 14:54:00 -0400

    correct more/less behavior when ther is a hyper link

--- app/assets/javascripts/more.js
+++ app/assets/javascripts/more.js
@@ -7,8 +7,8 @@ $(document).on('turbolinks:load', function() {
         var content = $(this).html();
  
         if(content.length > showChar) {
-            var c = content.substr(0, showChar);
-            var h = content.substr(showChar, content.length - showChar);
+            c = start_substr (content, showChar)
+            var h = content.substr(c.length, content.length - c.length);
             var html = c + '<span class="moreellipses">' + ellipsestext + '</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
  
             $(this).html(html);
@@ -26,4 +26,22 @@ $(document).on('turbolinks:load', function() {
         $(this).prev().toggle();
         return false;
     });
- });
+
+    function start_substr(string, length) {
+        var endtag = 0,i=0;
+        for(i; i<string.length; i++)
+        {
+          if(string[i] == ">")
+            endtag = i;
+        }
+
+        i = length;
+        if (endtag > length)
+        {
+           i = endtag;
+        }
+
+        var newString = string.substring(0,(i+1));
+        return newString;
+    }
+});
