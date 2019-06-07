9a044c2361361701b89a8a94ea7919f488d13f41
Fritz Freiheit <fritx@umich.edu>
2019-06-07 13:57:31 -0400

    BLUEDOC-388 - Create ticket in jira when work is created 3
    * Updated config to make localhost automatically be test mode for jira

--- config/application.rb
+++ config/application.rb
@@ -134,8 +134,11 @@ module DeepBlueDocs
                                           'testing.deepblue.lib.umich.edu',
                                           'staging.deepblue.lib.umich.edu',
                                           'deepblue.lib.umich.edu' ].freeze
+    config.jira_integration_hostnames_prod = [ 'testing.deepblue.lib.umich.edu',
+                                               'staging.deepblue.lib.umich.edu',
+                                               'deepblue.lib.umich.edu' ].freeze
     config.jira_integration_enabled = config.jira_integration_hostnames.include? config.hostname
-    config.jira_test_mode = true
+    config.jira_test_mode = !config.jira_integration_hostnames_prod.include?( config.hostname )
     # config.jira_manager_project_key = 'DBHELP'
     # config.jira_manager_issue_type = 'Data Deposit'
     config.jira_manager_project_key = 'BLUEDOC'
