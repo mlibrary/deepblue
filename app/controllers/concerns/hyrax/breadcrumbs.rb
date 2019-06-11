88bb8eae6fa52a237a975a32ee7a7505fd411927
Blanco <blancoj@umich.edu>
2019-06-11 12:32:25 -0400

    fix to breadscrumb

--- app/controllers/concerns/hyrax/breadcrumbs.rb
+++ app/controllers/concerns/hyrax/breadcrumbs.rb
@@ -37,7 +37,7 @@ module Hyrax
         add_breadcrumb I18n.t('hyrax.controls.home'), hyrax.root_path
         add_breadcrumb I18n.t('hyrax.bread_crumb.search_results'), request.referer
       else
-        default_trail
+        default_trail_start
         add_breadcrumb_for_controller if user_signed_in?
         add_breadcrumb_for_action
       end
