12d5940f4f9849086885c189e35e3c5e76f9d393
Fritz Freiheit <fritx@umich.edu>
2019-08-06 15:53:45 -0400

    BLUEDOC-605 - Don't show files in manage active embargoes list

--- config/application.rb
+++ config/application.rb
@@ -84,6 +84,7 @@ module DeepBlueDocs
     config.embargo_enforce_future_release_date = false
     config.embargo_visibility_after_default_status = ::Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
     config.embargo_visibility_during_default_status = ::Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE
+    config.embargo_manage_hide_files = true
 
     ## configure for Globus
     # -- To enable Globus for development, create /deepbluedata-globus/download and /deepbluedata-globus/prep
