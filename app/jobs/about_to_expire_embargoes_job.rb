27c62d05e940667e93b694dfdcdefdd0e44bd7de
Fritz Freiheit <fritx@umich.edu>
2019-10-21 15:24:50 -0400

    BLUEDOC-665 - Update scheduler jobs
    Fixed type in job_options_value method
    Updated user stat importer job as well

--- app/jobs/about_to_expire_embargoes_job.rb
+++ app/jobs/about_to_expire_embargoes_job.rb
@@ -20,15 +20,15 @@ class AboutToExpireEmbargoesJob < ::Hyrax::ApplicationJob
                                            "options=#{options}",
                                            Deepblue::LoggingHelper.obj_class( 'options', options ),
                                            "" ]
-    verbose = jop_options_value( options, key: 'verbose', default_value: false )
+    verbose = job_options_value(options, key: 'verbose', default_value: false )
     ::Deepblue::LoggingHelper.debug "verbose=#{verbose}" if verbose
-    email_owner = jop_options_value( options, key: 'email_owner', default_value: true )
+    email_owner = job_options_value(options, key: 'email_owner', default_value: true )
     ::Deepblue::LoggingHelper.debug "email_owner=#{email_owner}" if verbose
-    expiration_lead_days = jop_options_value( options, key: 'expiration_lead_days' )
+    expiration_lead_days = job_options_value(options, key: 'expiration_lead_days' )
     ::Deepblue::LoggingHelper.debug "expiration_lead_days=#{expiration_lead_days}" if verbose
-    skip_file_sets = jop_options_value( options, key: 'skip_file_sets', default_value: true )
+    skip_file_sets = job_options_value(options, key: 'skip_file_sets', default_value: true )
     ::Deepblue::LoggingHelper.debug "@skip_file_sets=#{skip_file_sets}" if verbose
-    test_mode = jop_options_value( options, key: 'test_mode', default_value: false )
+    test_mode = job_options_value(options, key: 'test_mode', default_value: false )
     ::Deepblue::LoggingHelper.debug "test_mode=#{test_mode}" if verbose
     ::Deepblue::AboutToExpireEmbargoesService.new( email_owner: email_owner,
                                                    expiration_lead_days: expiration_lead_days,
--- app/jobs/user_stat_importer_job.rb
+++ app/jobs/user_stat_importer_job.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class UserStatImporterJob < ::Hyrax::ApplicationJob
+  include JobHelper
   queue_as :scheduler
 
   def perform( *args )
@@ -15,18 +16,22 @@ class UserStatImporterJob < ::Hyrax::ApplicationJob
                                            "options=#{options}",
                                            Deepblue::LoggingHelper.obj_class( 'options', options ),
                                            "" ]
-    verbose = jop_options_value( options, key: 'verbose', default_value: false )
+    verbose = job_options_value(options, key: 'verbose', default_value: false )
     ::Deepblue::LoggingHelper.debug "verbose=#{verbose}" if verbose
-    logging = jop_options_value( options, key: 'logging', default_value: false )
-    ::Deepblue::LoggingHelper.debug "logging=#{logging}" if verbose
-    hostnames = jop_options_value( options, key: 'hostnames', default_value: [] )
-    ::Deepblue::LoggingHelper.debug "hostnames=#{hostnames}" if verbose
+    hostnames = job_options_value(options, key: 'hostnames', default_value: [], verbose: verbose )
     hostname = ::DeepBlueDocs::Application.config.hostname
     return unless hostnames.include? hostname
-    test = jop_options_value( options, key: 'test', default_value: true )
-    ::Deepblue::LoggingHelper.debug "test=#{test}" if verbose
-    return unless test
-    importer = Hyrax::UserStatImporter.new( verbose: verbose, logging: logging )
+    test = job_options_value(options, key: 'test', default_value: true, verbose: verbose )
+    echo_to_stdout = job_options_value(options, key: 'echo_to_stdout', default_value: false, verbose: verbose )
+    logging = job_options_value(options, key: 'logging', default_value: false, verbose: verbose )
+    number_of_retries = job_options_value(options, key: 'number_of_retries', default_value: nil, verbose: verbose )
+    delay_secs = job_options_value(options, key: 'delay_secs', default_value: nil, verbose: verbose )
+    importer = Hyrax::UserStatImporter.new( echo_to_stdout: echo_to_stdout,
+                                            verbose: verbose,
+                                            delay_secs: delay_secs,
+                                            logging: logging,
+                                            number_of_retries: number_of_retries,
+                                            test: test )
     importer.import
   rescue Exception => e # rubocop:disable Lint/RescueException
     Rails.logger.error "#{e.class} #{e.message} at #{e.backtrace[0]}"
