4b2d26c3e7211a6e535ee5313f8fa8f75d32c6fc
blancoj <blancoj@umich.edu>
2019-11-22 14:18:10 -0500

    remove default value setting.

--- db/migrate/20191122185851_change_allows_access.rb
+++ db/migrate/20191122185851_change_allows_access.rb
@@ -1,11 +1,8 @@
 class ChangeAllowsAccess < ActiveRecord::Migration[5.0]
   def change
-    change_column :sipity_workflows, :allows_access_grant, :boolean, :default => true
-
     Sipity::Workflow.find_each do |wf|
       wf.allows_access_grant = true
       wf.save!
     end
-
   end
 end
