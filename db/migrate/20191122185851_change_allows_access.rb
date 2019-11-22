c2c1b740b66d3aba365a1e8916918357b6409f35
Blanco <blancoj@umich.edu>
2019-11-22 15:02:10 -0500

    more more fix to migration.  only update the db values.

--- db/migrate/20191122185851_change_allows_access.rb
+++ db/migrate/20191122185851_change_allows_access.rb
@@ -1,5 +1,8 @@
 class ChangeAllowsAccess < ActiveRecord::Migration[5.0]
   def change
-
+    Sipity::Workflow.find_each do |wf|
+      wf.allows_access_grant = true
+      wf.save!
+    end
   end
 end
