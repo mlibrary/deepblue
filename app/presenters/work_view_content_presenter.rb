b1b674888d552c4101e863ecd899ae9702aa227e
Fritz Freiheit <fritx@umich.edu>
2020-02-12 13:34:32 -0500

    BLUEDOC-731 - Create work_view_content controller 2
    Changed to use data_send and added image handling.

--- app/presenters/work_view_content_presenter.rb
+++ app/presenters/work_view_content_presenter.rb
@@ -4,10 +4,16 @@ class WorkViewContentPresenter
 
   attr_accessor :controller
 
-  delegate :file_name, :work_title, to: :controller
+  # delegate :file_name, :work_title, :send_static_content, to: :controller
 
-  def initialize( controller: )
+  def initialize( controller:, file_set:, format: )
     @controller = controller
+    @file_set = file_set
+    @format = format
+  end
+
+  def static_content
+    @controller.send_static_content( file_set: @file_set, format: @format )
   end
 
 end
