15b3e3a268e2fbf51c72e2a9b2dbfa7274839c54
Fritz Freiheit <fritx@umich.edu>
2020-03-31 07:30:33 -0400

    Update uptime_service_integration.rb

--- config/initializers/uptime_service_integration.rb
+++ config/initializers/uptime_service_integration.rb
@@ -4,7 +4,11 @@ Deepblue::UptimeService.setup do |config|
   config.load_timestamp = DeepBlueDocs::Application.config.load_timestamp
   config.program_name = DeepBlueDocs::Application.config.program_name
 
-  Dir.mkdir( config.uptime_dir ) unless Dir.exists? config.uptime_dir
+  begin
+    Dir.mkdir( config.uptime_dir ) unless Dir.exists? config.uptime_dir
+  rescue Exception => e # rubocop:disable Lint/RescueException
+    # ignore, this will happen during moku build and deployment
+  end
 
   config.uptime_timestamp_file_write
 
