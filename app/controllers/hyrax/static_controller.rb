0303d42a8bdf9927642d83ee9aec7819f5d5d205
Fritz Freiheit <fritx@umich.edu>
2020-04-27 11:56:14 -0400

    BLUEDOC-809 - Route to internal documentation for static pages 4

--- app/controllers/hyrax/static_controller.rb
+++ app/controllers/hyrax/static_controller.rb
@@ -6,22 +6,66 @@ module Hyrax
   class StaticController < ApplicationController
     include Deepblue::StaticContentControllerBehavior
 
+    class_attribute :presenter_class
+    self.presenter_class = WorkViewContentPresenter
+
     STATIC_CONTROLLER_DEBUG_VERBOSE = false
 
     layout 'homepage'
 
-    attr_reader :file_id,
-                :file_name,
-                :format,
-                :menu,
-                :menu_file_format,
-                :menu_header,
-                :menu_links,
-                :menu_partial,
-                :page_navigation,
-                :work_title
-
     # TODO: figure out how to redirect to /data/work_view_content as required from this controller
+
+
+    def show
+      ::Deepblue::LoggingHelper.bold_debug [ ::Deepblue::LoggingHelper.here,
+                                             ::Deepblue::LoggingHelper.called_from,
+                                             "params=#{params}",
+                                             "params[:doc]=#{params[:doc]}",
+                                             "" ] if STATIC_CONTROLLER_DEBUG_VERBOSE
+
+      doc = params[:doc]
+      # if doc =~ %r{
+      #             about|
+      #             agreement|
+      #             dbd-documentation-guide|
+      #             dbd-glossary|
+      #             file-format-preservation|
+      #             globus-help|
+      #             help|
+      #             how-to-upload|
+      #             management-plan-text|
+      #             mendeley|
+      #             metadata-guidance|
+      #             prepare-your-data|
+      #             retention|
+      #             subject_libraries|
+      #             support-for-depositors|
+      #             terms|
+      #             use-downloaded-data|
+      #             versions|
+      #             zotero
+      #             }x
+      prefix = documentation_work_title_prefix
+      work_title = "#{prefix}#{doc}"
+      file_name = "#{doc}.html"
+      file_set = static_content_find_documentation_file_set( work_title: work_title, file_name: file_name )
+      if file_set.present?
+        if ::Deepblue::WorkViewContentService.static_controller_redirect_to_work_view_content
+          redirect_to( "/data/work_view_content/#{prefix}#{doc}/#{doc}.html" )
+        else
+          show_static_content_doc( work_title: work_title, file_name: file_name, file_set: file_set )
+        end
+      elsif static_content_file_set( "DBDDocumentation", "#{doc}.html" ).present?
+        redirect_to( "/data/work_view_content/DBDDocumentation/#{doc}.html" )
+      elsif static_content_file_set( "#{prefix}#{doc}", "#{doc}.html" ).present?
+        redirect_to( "/data/work_view_content/#{prefix}#{doc}/#{doc}.html" )
+      else
+        render "hyrax/static/#{doc}"
+      end
+      # else
+      #   redirect_to( main_app.root_path, status: 404 )
+      # end
+    end
 
     def show_doc
       ::Deepblue::LoggingHelper.bold_debug [ ::Deepblue::LoggingHelper.here,
@@ -56,75 +100,20 @@ module Hyrax
       render "show"
     end
 
-    def show
+    def show_static_content_doc( work_title:, file_name:, file_set: )
       ::Deepblue::LoggingHelper.bold_debug [ ::Deepblue::LoggingHelper.here,
                                              ::Deepblue::LoggingHelper.called_from,
-                                             "params=#{params}",
-                                             "params[:doc]=#{params[:doc]}",
-                                             "" ] if STATIC_CONTROLLER_DEBUG_VERBOSE
-
-      doc = params[:doc]
-      if doc =~ %r{
-                  about|
-                  agreement|
-                  dbd-documentation-guide|
-                  dbd-glossary|
-                  file-format-preservation|
-                  globus-help|
-                  help|
-                  how-to-upload|
-                  management-plan-text|
-                  mendeley|
-                  metadata-guidance|
-                  prepare-your-data|
-                  retention|
-                  subject_libraries|
-                  support-for-depositors|
-                  terms|
-                  use-downloaded-data|
-                  versions|
-                  zotero
-                  }x
-        # TODO: make this more efficient, probably start with DBDDocumentationCollection
-        if static_content_file_set( "DBDDocumentation", "#{doc}.html" ).present?
-          redirect_to( "/data/work_view_content/DBDDocumentation/#{doc}.html" )
-        elsif static_content_file_set( "DBDDoc-#{doc}", "#{doc}.html" ).present?
-          redirect_to( "/data/work_view_content/DBDDoc-#{doc}/#{doc}.html" )
-        else
-          render "hyrax/static/#{doc}"
-        end
-      else
-        redirect_to( main_app.root_path, status: 404 )
-      end
-    end
-
-    def show2
-      @work_title = params[:id]
-      @file_id = params[:file_id]
-      @file_name = @file_id
-      @format = params[:format]
-      @file_name = "#{file_name}.#{format}" unless format.empty?
-      ::Deepblue::LoggingHelper.bold_debug [ ::Deepblue::LoggingHelper.here,
-                                             ::Deepblue::LoggingHelper.called_from,
-                                             "params=#{params}",
                                              "work_title=#{work_title}",
-                                             "file_id=#{file_name}",
                                              "file_name=#{file_name}",
-                                             "format=#{format}",
-                                             "" ] if work_view_content_controller_debug_verbose
-      file_set = static_content_file_set( work_title, file_name )
-      ::Deepblue::LoggingHelper.bold_debug [ ::Deepblue::LoggingHelper.here,
-                                             ::Deepblue::LoggingHelper.called_from,
-                                             ::Deepblue::LoggingHelper.obj_class( "file_set", file_set),
                                              "file_set=#{file_set}",
-                                             "" ] if work_view_content_controller_debug_verbose
+                                             "" ] if STATIC_CONTROLLER_DEBUG_VERBOSE
       mime_type = file_set.mime_type if file_set.present?
-      options = options_from( file_set: file_set )
+      options = static_content_options_from( file_set: file_set, work_title: work_title )
       ::Deepblue::LoggingHelper.bold_debug [ ::Deepblue::LoggingHelper.here,
                                              ::Deepblue::LoggingHelper.called_from,
                                              "options=#{options}",
-                                             "" ] if work_view_content_controller_debug_verbose
-      if render_static_content? mime_type: mime_type
+                                             "" ] if STATIC_CONTROLLER_DEBUG_VERBOSE
+      if static_content_render? mime_type: mime_type
         @presenter = presenter_class.new( controller: self, file_set: file_set, format: format, options: options )
         render_with = options[:render_with]
         if render_with.present?
@@ -133,9 +122,10 @@ module Hyrax
           render 'hyrax/static/work_view_content'
         end
       else
-        send_static_content( file_set: file_set, format: format, options: options )
+        static_content_send( file_set: file_set, format: format, options: options )
       end
     end
+
 
     def zotero
       respond_to do |format|
--- config/initializers/work_view_content.rb
+++ config/initializers/work_view_content.rb
@@ -2,5 +2,7 @@
 Deepblue::WorkViewContentService.setup do |config|
 
   config.documentation_collection_title = "DBDDocumentationCollection"
+  config.documentation_work_title_prefix = "DBDDoc-"
+  config.static_controller_redirect_to_work_view_content = true
 
 end
