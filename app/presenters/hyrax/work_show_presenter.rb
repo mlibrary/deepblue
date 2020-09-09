ffc015f53023e1dad35a1dd228ef8e3796ce4eb2
Fritz Freiheit <fritx@umich.edu>
2020-09-09 11:57:29 -0400

    BLUEDOC-918 - Embargo/Private works give error for Single-Use Link
    BLUEDOC-918 - Embargo/Private works give error for Single-Use Link
    
    2nd commit to this branch

--- app/presenters/hyrax/work_show_presenter.rb
+++ app/presenters/hyrax/work_show_presenter.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require File.join(Gem::Specification.find_by_name("hyrax").full_gem_path, "app/presenters/hyrax/work_show_presenter.rb")
+# require File.join(Gem::Specification.find_by_name("hyrax").full_gem_path, "app/presenters/hyrax/work_show_presenter.rb")
 
 # monkey patch Hyrax::WorkShowPresenter
 module Hyrax
@@ -8,6 +8,46 @@ module Hyrax
   class WorkShowPresenter
 
     WORK_SHOW_PRESENTER_DEBUG_VERBOSE = ::DeepBlueDocs::Application.config.work_show_presenter_debug_verbose
+
+    include ModelProxy
+    include PresentsAttributes
+
+    attr_accessor :solr_document, :current_ability, :request
+
+    class_attribute :collection_presenter_class
+
+    # modify this attribute to use an alternate presenter class for the collections
+    self.collection_presenter_class = CollectionPresenter
+
+    # Methods used by blacklight helpers
+    delegate :has?, :first, :fetch, :export_formats, :export_as, to: :solr_document
+
+    # delegate fields from Hyrax::Works::Metadata to solr_document
+    delegate :based_near_label, :related_url, :depositor, :identifier, :resource_type,
+             :keyword, :itemtype, :admin_set, to: :solr_document
+
+    # @param [SolrDocument] solr_document
+    # @param [Ability] current_ability
+    # @param [ActionDispatch::Request] request the http request context. Used so
+    #                                  the GraphExporter knows what URLs to draw.
+    def initialize(solr_document, current_ability, request = nil)
+      ::Deepblue::LoggingHelper.bold_debug [ ::Deepblue::LoggingHelper.here,
+                                             ::Deepblue::LoggingHelper.called_from,
+                                             "" ] if WORK_SHOW_PRESENTER_DEBUG_VERBOSE
+      @solr_document = solr_document
+      @current_ability = current_ability
+      @request = request
+    end
+
+    # CurationConcern methods
+    delegate :stringify_keys, :human_readable_type, :collection?, :to_s,
+             to: :solr_document
+
+    # Metadata Methods
+    delegate :title, :date_created, :description,
+             :creator, :contributor, :subject, :publisher, :language, :embargo_release_date,
+             :lease_expiration_date, :license, :source, :rights_statement, :thumbnail_id, :representative_id,
+             :rendering_ids, :member_of_collection_ids, to: :solr_document
 
     attr_accessor :cc_single_use_link
 
@@ -336,10 +376,263 @@ module Hyrax
       return rv
     end
 
+
+    def workflow
+      # @workflow ||= WorkflowPresenter.new(solr_document, current_ability)
+      @workflow ||= workflow_init
+    end
+
+    def workflow_init
+      ::Deepblue::LoggingHelper.bold_debug [ ::Deepblue::LoggingHelper.here,
+                                             ::Deepblue::LoggingHelper.called_from,
+                                             "" ] if WORK_SHOW_PRESENTER_DEBUG_VERBOSE
+      rv = WorkflowPresenter.new(solr_document, current_ability)
+      ::Deepblue::LoggingHelper.bold_debug [ ::Deepblue::LoggingHelper.here,
+                                             ::Deepblue::LoggingHelper.called_from,
+                                             "" ] if WORK_SHOW_PRESENTER_DEBUG_VERBOSE
+      return rv
+    end
+
+    def inspect_work
+      # @inspect_workflow ||= InspectWorkPresenter.new(solr_document, current_ability)
+      @inspect_workflow ||= inspect_work_init
+    end
+
+    def inspect_work_init
+      ::Deepblue::LoggingHelper.bold_debug [ ::Deepblue::LoggingHelper.here,
+                                             ::Deepblue::LoggingHelper.called_from,
+                                             "" ] if WORK_SHOW_PRESENTER_DEBUG_VERBOSE
+      rv = InspectWorkPresenter.new(solr_document, current_ability)
+      ::Deepblue::LoggingHelper.bold_debug [ ::Deepblue::LoggingHelper.here,
+                                             ::Deepblue::LoggingHelper.called_from,
+                                             "" ] if WORK_SHOW_PRESENTER_DEBUG_VERBOSE
+      return rv
+    end
+
+    # @return [String] a download URL, if work has representative media, or a blank string
+    def download_url
+      return '' if representative_presenter.nil?
+      Hyrax::Engine.routes.url_helpers.download_url(representative_presenter, host: request.host)
+    end
+
+    # @return [Boolean] render a IIIF viewer
+    def iiif_viewer?
+      representative_id.present? &&
+          representative_presenter.present? &&
+          representative_presenter.image? &&
+          Hyrax.config.iiif_image_server? &&
+          members_include_viewable_image?
+    end
+
+    alias universal_viewer? iiif_viewer?
+    deprecation_deprecate universal_viewer?: "use iiif_viewer? instead"
+
+    # @return [Symbol] the name of the IIIF viewer partial to render
+    # @example A work presenter with a custom iiif viewer
+    #   module Hyrax
+    #     class GenericWorkPresenter < Hyrax::WorkShowPresenter
+    #       def iiif_viewer
+    #         :my_iiif_viewer
+    #       end
+    #     end
+    #   end
+    #
+    #   Custom iiif viewer partial at app/views/hyrax/base/iiif_viewers/_my_iiif_viewer.html.erb
+    #   <h3>My IIIF Viewer!</h3>
+    #   <a href=<%= main_app.polymorphic_url([main_app, :manifest, presenter], { locale: nil }) %>>Manifest</a>
+    def iiif_viewer
+      :universal_viewer
+    end
+
+    # Get presenters for the collections this work is a member of via the member_of_collections association.
+    # @return [Array<CollectionPresenter>] presenters
+    def member_of_collection_presenters
+      PresenterFactory.build_for(ids: member_of_authorized_parent_collections,
+                                 presenter_class: collection_presenter_class,
+                                 presenter_args: presenter_factory_arguments)
+    end
+
+    def date_modified
+      solr_document.date_modified.try(:to_formatted_s, :standard)
+    end
+
+    def date_uploaded
+      solr_document.date_uploaded.try(:to_formatted_s, :standard)
+    end
+
+    def link_name
+      current_ability.can?(:read, id) ? to_s : 'File'
+    end
+
+    def export_as_nt
+      graph.dump(:ntriples)
+    end
+
+    def export_as_jsonld
+      graph.dump(:jsonld, standard_prefixes: true)
+    end
+
+    def export_as_ttl
+      graph.dump(:ttl)
+    end
+
+    def editor?
+      return false if single_use_show?
+      current_ability.can?(:edit, solr_document)
+    end
+
+    def tweeter
+      TwitterPresenter.twitter_handle_for(user_key: depositor)
+    end
+
+    def presenter_types
+      Hyrax.config.registered_curation_concern_types.map(&:underscore) + ["collection"]
+    end
+
+    # @return [Array] presenters grouped by model name, used to show the parents of this object
+    def grouped_presenters(filtered_by: nil, except: nil)
+      # TODO: we probably need to retain collection_presenters (as parent_presenters)
+      #       and join this with member_of_collection_presenters
+      grouped = member_of_collection_presenters.group_by(&:model_name).transform_keys { |key| key.to_s.underscore }
+      grouped.select! { |obj| obj.downcase == filtered_by } unless filtered_by.nil?
+      grouped.except!(*except) unless except.nil?
+      grouped
+    end
+
+    def work_featurable?
+      user_can_feature_works? && solr_document.public?
+    end
+
+    def display_feature_link?
+      work_featurable? && FeaturedWork.can_create_another? && !featured?
+    end
+
+    def display_unfeature_link?
+      work_featurable? && featured?
+    end
+
+    def stats_path
+      Hyrax::Engine.routes.url_helpers.stats_work_path(self, locale: I18n.locale)
+    end
+
+    def model
+      solr_document.to_model
+    end
+
+    delegate :member_presenters, :ordered_ids, :file_set_presenters, :work_presenters, to: :member_presenter_factory
+
+    # @return [Array] list to display with Kaminari pagination
+    def list_of_item_ids_to_display
+      paginated_item_list(page_array: authorized_item_ids)
+    end
+
+    # @return [Integer] total number of pages of viewable items
+    def total_pages
+      (total_items.to_f / rows_from_params.to_f).ceil
+    end
+
+    def manifest_url
+      manifest_helper.polymorphic_url([:manifest, self])
+    end
+
+    # IIIF rendering linking property for inclusion in the manifest
+    #  Called by the `iiif_manifest` gem to add a 'rendering' (eg. a link a download for the resource)
+    #
+    # @return [Array] array of rendering hashes
+    def sequence_rendering
+      renderings = []
+      if solr_document.rendering_ids.present?
+        solr_document.rendering_ids.each do |file_set_id|
+          renderings << manifest_helper.build_rendering(file_set_id)
+        end
+      end
+      renderings.flatten
+    end
+
+    # IIIF metadata for inclusion in the manifest
+    #  Called by the `iiif_manifest` gem to add metadata
+    #
+    # @return [Array] array of metadata hashes
+    def manifest_metadata
+      metadata = []
+      Hyrax.config.iiif_metadata_fields.each do |field|
+        metadata << {
+            'label' => I18n.t("simple_form.labels.defaults.#{field}"),
+            'value' => Array.wrap(send(field))
+        }
+      end
+      metadata
+    end
+
+    # determine if the user can add this work to a collection
+    # @param collections <Collections> list of collections to which this user can deposit
+    # @return true if the user can deposit to at least one collection OR if the user can create a collection; otherwise, false
+    def show_deposit_for?(collections:)
+      collections.present? || current_ability.can?(:create_any, Collection)
+    end
+
     private
 
       def link_presenter_class
         SingleUseLinkPresenter
+      end
+
+      # list of item ids to display is based on ordered_ids
+      def authorized_item_ids
+        @member_item_list_ids ||= begin
+          items = ordered_ids
+          items.delete_if { |m| !current_ability.can?(:read, m) } if Flipflop.hide_private_items?
+          items
+        end
+      end
+
+      # Uses kaminari to paginate an array to avoid need for solr documents for items here
+      def paginated_item_list(page_array:)
+        Kaminari.paginate_array(page_array, total_count: page_array.size).page(current_page).per(rows_from_params)
+      end
+
+      def total_items
+        authorized_item_ids.size
+      end
+
+      def rows_from_params
+        request.params[:rows].nil? ? Hyrax.config.show_work_item_rows : request.params[:rows].to_i
+      end
+
+      def current_page
+        page = request.params[:page].nil? ? 1 : request.params[:page].to_i
+        page > total_pages ? total_pages : page
+      end
+
+      def manifest_helper
+        @manifest_helper ||= ManifestHelper.new(request.base_url)
+      end
+
+      def featured?
+        @featured = FeaturedWork.where(work_id: solr_document.id).exists? if @featured.nil?
+        @featured
+      end
+
+      def user_can_feature_works?
+        return false if single_use_show?
+        current_ability.can?(:create, FeaturedWork)
+      end
+
+      def presenter_factory_arguments
+        [current_ability, request]
+      end
+
+      def graph
+        GraphExporter.new(solr_document, request).fetch
+      end
+
+      def member_of_authorized_parent_collections
+        # member_of_collection_ids with current_ability access
+        @member_of ||= Hyrax::CollectionMemberService.run(solr_document, current_ability).map(&:id)
+      end
+
+      def members_include_viewable_image?
+        file_set_presenters.any? { |presenter| presenter.image? && current_ability.can?(:read, presenter.id) }
       end
 
   end
