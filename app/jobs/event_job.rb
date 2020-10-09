966d2832816eb1a3fe1a8f679fbcd21dd0be391e
Fritz Freiheit <fritx@umich.edu>
2020-10-09 09:31:35 -0400

    BLUEDOC-940 - Add transfer of works to provenance log - 2

---
+++ app/jobs/event_job.rb
@@ -0,0 +1,36 @@
+# A generic job for sending events to a user.
+#
+# @abstract
+class EventJob < Hyrax::ApplicationJob
+  include Rails.application.routes.url_helpers
+  # For link_to
+  include ActionView::Helpers
+  # For link_to_profile
+  include HyraxHelper
+
+  queue_as Hyrax.config.ingest_queue_name
+  attr_reader :depositor
+
+  # @param [User] depositor the user to create the event for
+  def perform(depositor)
+    @depositor = depositor
+    # Log the event to the depositor's profile stream
+    log_user_event(depositor)
+  end
+
+  # override to provide your specific action for the event you are logging
+  # @abstract
+  def action
+    raise(NotImplementedError, "#action should be implemented by an child class of EventJob")
+  end
+
+  # create an event with an action and a timestamp for the user
+  def event
+    @event ||= Hyrax::Event.create(action, Time.current.to_i)
+  end
+
+  # log the event to the users event stream
+  def log_user_event(depositor)
+    depositor.log_event(event)
+  end
+end
--- app/services/hyrax/change_content_depositor_service.rb
+++ app/services/hyrax/change_content_depositor_service.rb
@@ -1,18 +1,39 @@
+# frozen_string_literal: true
+
 module Hyrax
+
   class ChangeContentDepositorService
+
+    CHANGE_CONTENT_DEPOSITOR_SERVICE_DEBUG_VERBOSE = true
+
     # @param [ActiveFedora::Base] work
     # @param [User] user
     # @param [TrueClass, FalseClass] reset
-    def self.call(work, user, reset)
+    def self.call( work, user, reset )
+      ::Deepblue::LoggingHelper.bold_debug [ ::Deepblue::LoggingHelper.here,
+                                             ::Deepblue::LoggingHelper.called_from,
+                                             "work=#{work}",
+                                             "user=#{user}",
+                                             "reset=#{reset}",
+                                             "" ] if CHANGE_CONTENT_DEPOSITOR_SERVICE_DEBUG_VERBOSE
+      previous_user = ::User.find_by_user_key(work.depositor)
       work.proxy_depositor = work.depositor
       work.permissions = [] if reset
       work.apply_depositor_metadata(user)
       work.file_sets.each do |f|
         f.apply_depositor_metadata(user)
         f.save!
+        f.provenance_transfer( current_user: user,
+                               previous_user: previous_user,
+                               event_note: "reset=#{reset}" ) if f.respond_to? :provenance_transfer
       end
       work.save!
+      work.provenance_transfer( current_user: user,
+                                previous_user: previous_user,
+                                event_note: "reset=#{reset}" ) if work.respond_to? :provenance_transfer
       work
     end
+
   end
+
 end
