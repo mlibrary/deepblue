3c042b3c77857ea5076ca24e7f6214879b1c9bf9
Fritz Freiheit <fritx@umich.edu>
2021-04-26 08:08:02 -0400

    BLUEDOC-1084 - Upgrade Ahoy gem

--- lib/ahoy/tracker.rb
+++ lib/ahoy/tracker.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 # monkey add method ahoy_matey lib/ahoy/tracker.rb
+# https://github.com/ankane/ahoy/blob/master/lib/ahoy/tracker.rb
+# https://github.com/mlibrary/deepblue/blob/master/lib/ahoy/tracker.rb
 require File.join( Gem::Specification.find_by_name( "ahoy_matey" ).full_gem_path, "lib/ahoy/tracker.rb" )
 
 require_relative "../../app/services/deepblue/analytics_integration_service"
@@ -9,7 +11,7 @@ module Ahoy
 
   class Tracker
 
-    AHOY_TRACKER_DEBUG_VERBOSE = ::Deepblue::AnalyticsIntegrationService.ahoy_tracker_debug_verbose
+    mattr_accessor :ahoy_tracker_debug_verbose, default: ::Deepblue::AnalyticsIntegrationService.ahoy_tracker_debug_verbose
 
     def track( name, properties = {}, options = {} )
       cc_id = find_cc_id( name, properties )
@@ -30,7 +32,7 @@ module Ahoy
                                              "cc_id=#{cc_id}",
                                              "properties=#{properties}",
                                              "options=#{options}",
-                                             "" ] if AHOY_TRACKER_DEBUG_VERBOSE
+                                             "" ] if ahoy_tracker_debug_verbose
       if exclude?
         debug "Event excluded"
       elsif missing_params?
@@ -43,7 +45,7 @@ module Ahoy
             properties: properties,
             time: trusted_time(options[:time]),
             event_id: options[:id] || generate_id,
-            cc_id: cc_id
+            cc_id: cc_id  # monkey: add the curation concern id, only change other than debug above
         }.select { |_, v| v }
 
         @store.track_event(data)
