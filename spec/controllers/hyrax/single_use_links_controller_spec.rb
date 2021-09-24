d9234c2e075fd3142fe0a5b8c9c8160fda82d993
Fritz Freiheit <fritx@umich.edu>
2021-09-24 17:11:31 -0400

    BLUEDOC-1049-50 - Controller RSpec: single_use_links_controller/single_use_links_viewer_controller

--- spec/controllers/hyrax/single_use_links_controller_spec.rb
+++ spec/controllers/hyrax/single_use_links_controller_spec.rb
@@ -7,103 +7,193 @@ RSpec.describe Hyrax::SingleUseLinksController, type: :controller do
   let(:main_app) { Rails.application.routes.url_helpers }
   let(:hyrax) { Hyrax::Engine.routes.url_helpers }
 
+  let(:debug_verbose) { false }
+
   let(:user) { create(:user) }
   let(:file) { create(:file_set, user: user) }
 
-  describe "::show_presenter" do
-    subject { described_class }
-
-    its(:show_presenter) { is_expected.to eq(Hyrax::SingleUseLinkPresenter) }
+  describe 'module debug verbose variables' do
+    it "they have the right values" do
+      expect( described_class.single_use_links_controller_debug_verbose ).to eq( debug_verbose )
+    end
   end
 
-  describe "logged in user with edit permission" do
-    let(:hash) { "some-dummy-sha2-hash" }
-
-    before { sign_in user }
-
-    context "POST create" do
+  describe '#show_presenter' do
+    RSpec.shared_examples 'shared show_presenter' do |dbg_verbose|
+      subject { described_class }
       before do
-        expect(Digest::SHA2).to receive(:new).and_return(hash)
+        described_class.single_use_links_controller_debug_verbose = dbg_verbose
+        expect(::Deepblue::LoggingHelper).to receive(:bold_debug).at_least(:once) if dbg_verbose
+        expect(::Deepblue::LoggingHelper).to_not receive(:bold_debug) unless dbg_verbose
+      end
+      after do
+        described_class.single_use_links_controller_debug_verbose = debug_verbose
       end
 
-      describe "creating a single-use download link" do
-        it "returns a link for downloading" do
-          post 'create_download', params: { id: file }
-          expect(response).to be_success
-          expect(response.body).to eq Hyrax::Engine.routes.url_helpers.download_single_use_link_url(hash, host: request.host, locale: 'en')
+      its(:show_presenter) do
+        is_expected.to eq(Hyrax::SingleUseLinkPresenter)
+      end
+    end
+    it_behaves_like 'shared show_presenter', false
+    # it_behaves_like 'shared show_presenter', true # no debug statements to trigger
+  end
+
+  describe 'logged in user with edit permission' do
+    RSpec.shared_examples 'shared logged in user with edit permission' do |dbg_verbose|
+      before do
+        described_class.single_use_links_controller_debug_verbose = dbg_verbose
+        expect(::Deepblue::LoggingHelper).to receive(:bold_debug).at_least(:once) if dbg_verbose
+        expect(::Deepblue::LoggingHelper).to_not receive(:bold_debug) unless dbg_verbose
+      end
+      after do
+        described_class.single_use_links_controller_debug_verbose = debug_verbose
+      end
+      context do
+        let(:hash) { "some-dummy-sha2-hash" }
+
+        before { sign_in user }
+
+        context "POST create" do
+          before do
+            expect(Digest::SHA2).to receive(:new).and_return(hash)
+          end
+
+          describe "creating a single-use download link" do
+            it "returns a link for downloading" do
+              post 'create_download', params: { id: file }
+              expect(response).to be_success
+              expect(response.body).to eq hyrax.download_single_use_link_url(hash, host: request.host, locale: 'en')
+            end
+          end
+
+          describe "creating a single-use show link" do
+            it "returns a link for showing" do
+              post 'create_show', params: { id: file }
+              expect(response).to be_success
+              expect(response.body).to eq hyrax.show_single_use_link_url(hash, host: request.host, locale: 'en')
+            end
+          end
         end
-      end
 
-      describe "creating a single-use show link" do
-        it "returns a link for showing" do
-          post 'create_show', params: { id: file }
-          expect(response).to be_success
-          expect(response.body).to eq Hyrax::Engine.routes.url_helpers.show_single_use_link_url(hash, host: request.host, locale: 'en')
+        # TODO: fix
+        context "GET index", skip: false do
+          describe "viewing existing links" do
+            before { get :index, params: { id: file } }
+            subject { response }
+
+            it { is_expected.to be_success }
+          end
+        end
+
+        context "DELETE destroy" do
+          let!(:link) { create(:download_link) }
+
+          it "deletes the link" do
+            expect { delete :destroy, params: { id: file, link_id: link } }.to change { SingleUseLink.count }.by(-1)
+            expect(response).to be_success
+          end
         end
       end
     end
+    it_behaves_like 'shared logged in user with edit permission', false
+    it_behaves_like 'shared logged in user with edit permission', true
+  end
 
-    # TODO: fix
-    context "GET index", skip: true do
-      describe "viewing existing links" do
-        before { get :index, params: { id: file } }
+  describe 'logged in user without edit permission' do
+    RSpec.shared_examples 'shared logged in user without edit permission' do |dbg_verbose|
+      before do
+        described_class.single_use_links_controller_debug_verbose = dbg_verbose
+        expect(::Deepblue::LoggingHelper).to receive(:bold_debug).at_least(:once) if dbg_verbose
+        expect(::Deepblue::LoggingHelper).to_not receive(:bold_debug) unless dbg_verbose
+      end
+      after do
+        described_class.single_use_links_controller_debug_verbose = debug_verbose
+      end
+      context do
+        let(:other_user) { create(:user) }
+        let(:file) { create(:file_set, user: user, read_users: [other_user]) }
+
+        before do
+          sign_in other_user
+        end
+
         subject { response }
 
-        it { is_expected.to be_success }
+        describe 'creating a single-use download link' do
+          before { post 'create_download', params: { id: file } }
+          it 'fails' do
+            is_expected.not_to be_success
+            expect(response).to redirect_to(root_path)
+            expect(flash[:alert]).to eq I18n.t('hyrax.single_use_links.alert.insufficient_privileges')
+          end
+        end
+
+        describe 'creating a single-use show link' do
+          before { post 'create_show', params: { id: file } }
+          it 'fails' do
+            is_expected.not_to be_success
+            expect(response).to redirect_to(root_path)
+            expect(flash[:alert]).to eq I18n.t('hyrax.single_use_links.alert.insufficient_privileges')
+          end
+        end
+
+        describe 'viewing existing links' do
+          before { get :index, params: { id: file } }
+          it 'fails' do
+            is_expected.not_to be_success
+            expect(response).to redirect_to(root_path)
+            expect(flash[:alert]).to eq I18n.t('hyrax.single_use_links.alert.insufficient_privileges')
+          end
+        end
       end
     end
+    it_behaves_like 'shared logged in user without edit permission', false
+    it_behaves_like 'shared logged in user without edit permission', true
+  end
 
-    context "DELETE destroy" do
-      let!(:link) { create(:download_link) }
-
-      it "deletes the link" do
-        expect { delete :destroy, params: { id: file, link_id: link } }.to change { SingleUseLink.count }.by(-1)
-        expect(response).to be_success
+  describe 'unknown user' do
+    RSpec.shared_examples 'it requires login' do
+      let(:flash_msg) { "You need to sign in or sign up before continuing." }
+      # let(:flash_msg) { I18n.t('devise.failure.unauthenticated') }
+      # let(:flash_msg) { I18n.t(:"unauthorized.default", default: 'You are not authorized to access this page.') }
+      it 'requires login' do
+        expect(response).to_not be_nil
+        # expect(response).to fail_redirect_and_flash(main_app.new_user_session_path, flash_msg)
+        expect(response.status).to eq 302
+        expect(response).to redirect_to(main_app.new_user_session_path)
+        expect(flash[:alert]).to eq flash_msg
       end
     end
-  end
+    RSpec.shared_examples 'shared unknown user' do |dbg_verbose|
+      before do
+        described_class.single_use_links_controller_debug_verbose = dbg_verbose
+        expect(::Deepblue::LoggingHelper).to receive(:bold_debug).at_least(:once) if dbg_verbose
+        expect(::Deepblue::LoggingHelper).to_not receive(:bold_debug) unless dbg_verbose
+      end
+      after do
+        described_class.single_use_links_controller_debug_verbose = debug_verbose
+      end
+      context do
+        subject { response }
 
-  describe "logged in user without edit permission" do
-    let(:other_user) { create(:user) }
-    let(:file) { create(:file_set, user: user, read_users: [other_user]) }
+        describe "creating a single-use download link" do
+          before { post 'create_download', params: { id: file } }
+          it_behaves_like 'it requires login'
+        end
 
-    before { sign_in other_user }
-    subject { response }
+        describe "creating a single-use show link" do
+          before { post 'create_show', params: { id: file } }
+          it_behaves_like 'it requires login'
+        end
 
-    describe "creating a single-use download link" do
-      before { post 'create_download', params: { id: file } }
-      it { is_expected.not_to be_success }
+        describe "viewing existing links" do
+          before { get :index, params: { id: file } }
+          it_behaves_like 'it requires login'
+        end
+      end
     end
-
-    describe "creating a single-use show link" do
-      before { post 'create_show', params: { id: file } }
-      it { is_expected.not_to be_success }
-    end
-
-    describe "viewing existing links" do
-      before { get :index, params: { id: file } }
-      it { is_expected.not_to be_success }
-    end
-  end
-
-  describe "unknown user" do
-    subject { response }
-
-    describe "creating a single-use download link" do
-      before { post 'create_download', params: { id: file } }
-      it { is_expected.not_to be_success }
-    end
-
-    describe "creating a single-use show link" do
-      before { post 'create_show', params: { id: file } }
-      it { is_expected.not_to be_success }
-    end
-
-    describe "viewing existing links" do
-      before { get :index, params: { id: file } }
-      it { is_expected.not_to be_success }
-    end
-
+    it_behaves_like 'shared unknown user', false
+    # it_behaves_like 'shared unknown user', true # no debug statements to trigger
   end
 
 end
--- spec/controllers/hyrax/single_use_links_viewer_controller_spec.rb
+++ spec/controllers/hyrax/single_use_links_viewer_controller_spec.rb
@@ -7,74 +7,99 @@ RSpec.describe Hyrax::SingleUseLinksViewerController do
   let(:hyrax) { Hyrax::Engine.routes.url_helpers }
   routes { Hyrax::Engine.routes }
 
-  let(:user) { build(:user) }
-  let(:file) do
-    create(:file_set, label: 'world.png', user: user)
+  let(:debug_verbose) { false }
+
+  describe 'module debug verbose variables' do
+    it "they have the right values" do
+      expect( described_class.single_use_links_viewer_controller_debug_verbose ).to eq( debug_verbose )
+    end
   end
 
-  describe "retrieval links" do
-    let :show_link do
-      SingleUseLink.create itemId: file.id,
-                           path: Rails.application.routes.url_helpers.hyrax_file_set_path(id: file, locale: 'en')
+  describe 'retrieval links' do
+    let(:user) { build(:user) }
+    let(:file) do
+      create(:file_set, label: 'world.png', user: user)
     end
 
-    let :download_link do
-      Hydra::Works::AddFileToFileSet.call_enhanced_version( file, File.open(fixture_path + '/world.png'), :original_file )
-      SingleUseLink.create itemId: file.id, path: Hyrax::Engine.routes.url_helpers.download_path(id: file, locale: 'en')
-    end
-
-    let(:show_link_hash) { show_link.downloadKey }
-    let(:download_link_hash) { download_link.downloadKey }
-
-    describe "GET 'download'" do
-      let(:expected_content) { ActiveFedora::Base.find(file.id).original_file.content }
-
-      it "downloads the file and deletes the link from the database" do
-        expect(controller).to receive(:send_file_headers!).with( filename: 'world.png',
-                                                                 disposition: 'attachment',
-                                                                 type: 'image/png')
-        get :download, params: { id: download_link_hash }
-        expect(response.body).to eq expected_content
-        expect(response).to be_success
-        expect { SingleUseLink.find_by_downloadKey!(download_link_hash) }.to raise_error ActiveRecord::RecordNotFound
+    RSpec.shared_examples 'shared retrieval links' do |dbg_verbose|
+      before do
+        described_class.single_use_links_viewer_controller_debug_verbose = dbg_verbose
+        expect(::Deepblue::LoggingHelper).to receive(:bold_debug).at_least(:once) if dbg_verbose
+        expect(::Deepblue::LoggingHelper).to_not receive(:bold_debug) unless dbg_verbose
+        # ::Deepblue::LoggingHelper.echo_to_puts = true if !debug_verbose
       end
+      after do
+        described_class.single_use_links_viewer_controller_debug_verbose = debug_verbose
+        # ::Deepblue::LoggingHelper.echo_to_puts = false if !debug_verbose
+      end
+      context do
 
-      context "when the key is not found" do
-        before { SingleUseLink.find_by_downloadKey!(download_link_hash).destroy }
+        let :show_link do
+          SingleUseLink.create itemId: file.id,
+                               path: Rails.application.routes.url_helpers.hyrax_file_set_path(id: file, locale: 'en')
+        end
 
-        it "shows the main page with message" do
-          get :download, params: { id: download_link_hash }
-          expect(response).to redirect_to(root_path)
-          expect(flash[:alert]).to include(I18n.t('hyrax.single_use_links.expired_html'))
+        let :download_link do
+          Hydra::Works::AddFileToFileSet.call_enhanced_version( file, File.open(fixture_path + '/world.png'), :original_file )
+          SingleUseLink.create itemId: file.id, path: Hyrax::Engine.routes.url_helpers.download_path(id: file, locale: 'en')
+        end
+
+        let(:show_link_hash) { show_link.downloadKey }
+        let(:download_link_hash) { download_link.downloadKey }
+
+        describe "GET 'download'", skip: false do
+          let(:expected_content) { ActiveFedora::Base.find(file.id).original_file.content }
+
+          it "downloads the file and deletes the link from the database", skip: false do
+            expect(controller).to receive(:send_file_headers!).with( filename: 'world.png',
+                                                                     disposition: 'attachment',
+                                                                     type: 'image/png')
+            get :download, params: { id: download_link_hash }
+            expect(response.body).to eq expected_content
+            expect(response).to be_success
+            expect { SingleUseLink.find_by_downloadKey!(download_link_hash) }.to raise_error ActiveRecord::RecordNotFound
+          end
+
+          context "when the key is not found", skip: false do
+            before { SingleUseLink.find_by_downloadKey!(download_link_hash).destroy }
+
+            it "shows the main page with message" do
+              get :download, params: { id: download_link_hash }
+              expect(response).to redirect_to(root_path)
+              expect(flash[:alert]).to include(I18n.t('hyrax.single_use_links.expired_html'))
+            end
+          end
+        end
+
+        describe "GET 'show'", skip: false do
+
+          it "renders the file set's show page and deletes the link from the database" do
+            get 'show', params: { id: show_link_hash }
+            expect(response).to redirect_to( "http://test.host/concern/file_sets/#{file.id}/single_use_link/#{show_link_hash}" )
+            expect(flash[:notice]).to include(I18n.t('hyrax.single_use_links.notice.show_file_html'))
+            # expect(assigns[:presenter].id).to eq file.id
+            # expect { SingleUseLink.find_by_downloadKey!(show_link_hash) }.to raise_error ActiveRecord::RecordNotFound
+          end
+
+          context "shows the main page with message when the key is not found" do
+            before { SingleUseLink.find_by_downloadKey!(show_link_hash).destroy }
+            it "redirects to the main page" do
+              get :show, params: { id: show_link_hash }
+              expect(response).to redirect_to(root_path)
+              expect(flash[:alert]).to include(I18n.t('hyrax.single_use_links.expired_html'))
+            end
+          end
+
+          it "shows the main page with message when get show path with download hash" do
+            get :show, params: { id: download_link_hash }
+            expect(response).to redirect_to( "http://test.host/concern/file_sets/#{file.id}/single_use_link/#{download_link_hash}" )
+            expect(flash[:notice]).to include(I18n.t('hyrax.single_use_links.notice.show_file_html'))
+          end
         end
       end
     end
-
-    describe "GET 'show'" do
-
-      it "renders the file set's show page and deletes the link from the database" do
-        get 'show', params: { id: show_link_hash }
-        expect(response).to redirect_to( "http://test.host/concern/file_sets/#{file.id}/single_use_link/#{show_link_hash}" )
-        expect(flash[:notice]).to include(I18n.t('hyrax.single_use_links.notice.show_file_html'))
-        # expect(assigns[:presenter].id).to eq file.id
-        # expect { SingleUseLink.find_by_downloadKey!(show_link_hash) }.to raise_error ActiveRecord::RecordNotFound
-      end
-
-      context "shows the main page with message when the key is not found" do
-        before { SingleUseLink.find_by_downloadKey!(show_link_hash).destroy }
-        it "redirects to the main page" do
-          get :show, params: { id: show_link_hash }
-          expect(response).to redirect_to(root_path)
-          expect(flash[:alert]).to include(I18n.t('hyrax.single_use_links.expired_html'))
-        end
-      end
-
-      it "shows the main page with message when get show path with download hash" do
-        get :show, params: { id: download_link_hash }
-        expect(response).to redirect_to( "http://test.host/concern/file_sets/#{file.id}/single_use_link/#{download_link_hash}" )
-        expect(flash[:notice]).to include(I18n.t('hyrax.single_use_links.notice.show_file_html'))
-      end
-    end
+    it_behaves_like 'shared retrieval links', false
+    it_behaves_like 'shared retrieval links', true
 
   end
 
