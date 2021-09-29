35138e28829ee9e1685c5bb4b5ac3225029bf9ce
Blanco <blancoj@umich.edu>
2021-09-29 13:52:15 -0400

    more rspec tests.

--- spec/jobs/content_event_job_spec.rb
+++ spec/jobs/content_event_job_spec.rb
@@ -1,7 +1,46 @@
 require 'rails_helper'
 
+class TestContentEventJob < ContentEventJob
+end
+
 RSpec.describe ContentEventJob do
-  it "is TODO" do
-    skip "the test code goes here"
+
+  let(:dummy_class) { double }
+  let(:dummy2_class) { double }
+
+  it "logs event" do
+  	allow(dummy_class).to receive(:log_event).and_return true
+    event_job = TestContentEventJob.new dummy_class, dummy2_class
+   	allow(event_job).to receive(:action).and_return "create"
+   	allow(dummy2_class).to receive(:log_profile_event).and_return true
+   	allow(Hyrax::Event ).to receive(:create).and_return "create"
+
+    dc = event_job.perform dummy_class, dummy2_class
+
+    expect(dc).to eq(true)
   end
+
+
+  it "logs event" do
+  	allow(dummy_class).to receive(:log_event).and_return true
+    event_job = TestContentEventJob.new dummy_class, dummy2_class
+   	allow(event_job).to receive(:action).and_return "create"
+   	allow(Hyrax::Event ).to receive(:create).and_return "create"
+
+    dc = event_job.log_event dummy_class
+
+    expect(dc).to eq(true)
+  end
+
+  it "logs user event" do
+  	allow(dummy_class).to receive(:log_profile_event).and_return true
+    event_job = TestContentEventJob.new dummy_class, dummy2_class
+   	allow(event_job).to receive(:action).and_return "create"
+   	allow(Hyrax::Event ).to receive(:create).and_return "create"
+
+    dc = event_job.log_user_event dummy_class
+
+    expect(dc).to eq(true)
+  end
+
 end
--- spec/presenters/hyrax/deepblue_presenter_spec.rb
+++ spec/presenters/hyrax/deepblue_presenter_spec.rb
@@ -2,10 +2,62 @@
 
 require 'rails_helper'
 
-RSpec.describe Hyrax::PresentsAttributes do
+class TestDeepbluePresenter < Hyrax::DeepbluePresenter
+end
 
-  it "is TODO" do
-    skip "the test code goes here"
+RSpec.describe Hyrax::DeepbluePresenter do
+
+  let(:dummy_class) { double }
+  let(:dummy2_class) { double }
+
+  it "checks if box is enabled" do
+    presenter = TestDeepbluePresenter.new dummy_class, dummy2_class
+    dc = presenter.box_enabled? 
+
+    expect(dc).to eq(false)
+  end
+
+  it "checks if provenance log is enabled" do
+    presenter = TestDeepbluePresenter.new dummy_class, dummy2_class
+    dc = presenter.display_provenance_log_enabled?
+
+    expect(dc).to eq(false)
+  end
+
+  it "checks if doi minting is enabled" do
+    presenter = TestDeepbluePresenter.new dummy_class, dummy2_class
+    dc = presenter.doi_minting_enabled?
+
+    expect(dc).to eq(false)
+  end
+
+  it "checks if globus download is enabled" do
+    presenter = TestDeepbluePresenter.new dummy_class, dummy2_class
+    dc = presenter.globus_download_enabled?
+
+    expect(dc).to eq(false)
+  end
+
+  it "get human readable types" do
+    presenter = TestDeepbluePresenter.new dummy_class, dummy2_class
+    dc = presenter.human_readable_type
+
+    expect(dc).to eq("Work")
+  end
+
+  xit "get human readable types" do
+    presenter = TestDeepbluePresenter.new dummy_class, dummy2_class
+    allow(MemberPresenterFactory).to receive(:new).and_return true
+    dc = presenter.member_presenter_factory
+
+    expect(dc).to eq(true)
+  end
+
+  it "get permission status for tombston hack" do
+    presenter = TestDeepbluePresenter.new dummy_class, dummy2_class
+    dc = presenter.tombstone_permissions_hack?
+
+    expect(dc).to eq(false)
   end
 
 end
