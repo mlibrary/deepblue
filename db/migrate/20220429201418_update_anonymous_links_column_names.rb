7c520fa056ce79ca2b67aaa6a31efa6000b1b8b1
Fritz Freiheit <fritx@umich.edu>
2022-05-18 13:40:54 -0400

    Update 20220429201418_update_anonymous_links_column_names.rb

--- db/migrate/20220429201418_update_anonymous_links_column_names.rb
+++ db/migrate/20220429201418_update_anonymous_links_column_names.rb
@@ -1,13 +1,25 @@
 class UpdateAnonymousLinksColumnNames < ActiveRecord::Migration[5.2]
   def change
-    # remove_index :anonymous_links, :downloadKey
-    # remove_index :anonymous_links, :itemId
+    begin
+      remove_index :anonymous_links, :downloadKey
+    rescue Exception => ignore  
+    end
+    begin
+      remove_index :anonymous_links, :itemId
+    rescue Exception => ignore
+    end
 
     rename_column :anonymous_links, :downloadKey, :download_key
     rename_column :anonymous_links, :itemId, :item_id
 
-    add_index :anonymous_links, :download_key
-    add_index :anonymous_links, :item_id
+    begin
+      add_index :anonymous_links, :download_key
+    rescue Exception => ignore
+    end
+    begin
+      add_index :anonymous_links, :item_id
+    rescue Exception => ignore
+    end
   end
 
 end
