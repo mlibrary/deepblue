5fc6150543aa02bf4e16823b2e44ef7a1e30a5f3
Fritz Freiheit <fritx@umich.edu>
2022-05-18 13:10:31 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,10 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.configure do |env|
+    env.logger = Rails.logger
+  end
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
