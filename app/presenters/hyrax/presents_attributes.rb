69a928254cae9d5edb08c85aca97b47663eda011
Blanco <blancoj@umich.edu>
2022-05-20 15:49:17 -0400

    make more than 5 authors show up as para in work page.

--- app/presenters/hyrax/presents_attributes.rb
+++ app/presenters/hyrax/presents_attributes.rb
@@ -82,15 +82,15 @@ module Hyrax
     # monkey
     def present_authors_compact
       authors = attribute_to_html(:creator, render_as: :faceted, label: I18n.t('show.labels.creator') )
-      author.gsub!('itemscope itemtype="http://schema.org/Person"', '')
-            .gsub!('<td>', '')
-            .gsub!('</td>', '')
-            .gsub!('<tr>', '')
-            .gsub!('</tr>', '')
-            .gsub!(/<th.*?>(.+?)<\/th>/, '')
-            .gsub!(/<ul.*?>(.+?)<\/ul>/, '\1')
-            .gsub!(/<li.*?>(.+?)<\/li>/, '\1|  ')
-            .gsub!(/<span.*?>(.+?)<\/span>/, '\1')
+      authors.gsub!('itemscope itemtype="http://schema.org/Person"', '')
+             .gsub!('<td>', '')
+             .gsub!('</td>', '')
+             .gsub!('<tr>', '')
+             .gsub!('</tr>', '')
+             .gsub!(/<th.*?>(.+?)<\/th>/, '')
+             .gsub!(/<ul.*?>(.+?)<\/ul>/, '\1')
+             .gsub!(/<li.*?>(.+?)<\/li>/, '\1|  ')
+             .gsub!(/<span.*?>(.+?)<\/span>/, '\1')
       authors = authors.reverse.sub('|', '').sub('|', ' dna ').reverse.gsub!('|', ';')
       "<span class=\"moreauthor\">#{authors}</span>"
     end
