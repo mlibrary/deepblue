39470d48ccaaf4671b04be55d583d397dcfb40e4
Fritz Freiheit <fritx@umich.edu>
2023-02-01 15:14:52 -0500

    DEEPBLUE-193 - Update TeamDynamix API - 1a

--- spec/services/deepblue/teamdynamix_service_spec.rb
+++ spec/services/deepblue/teamdynamix_service_spec.rb
@@ -12,15 +12,14 @@ RSpec.describe ::Deepblue::TeamdynamixService do
     it { expect( described_class.build_data_debug_verbose ).to eq debug_verbose }
     it { expect( described_class.build_headers_debug_verbose ).to eq debug_verbose }
     it { expect( described_class.get_ticket_body_debug_verbose ).to eq debug_verbose }
-    it { expect( described_class.esponse_debug_verbose ).to eq debug_verbose }
+    it { expect( described_class.response_debug_verbose ).to eq debug_verbose }
   end
 
   describe 'module variables have the expected values' do
     it { expect( described_class.check_admin_notes_for_existing_ticket ).to eq true }
     it { expect( described_class.include_attributes_in_update ).to eq false }
     it { expect( described_class.include_ibm_client_id ).to eq false }
-    it { expect( described_class.build_access_token_parms ).to
-                  eq '/um/oauth2/token?scope=tdxticket&grant_type=client_credentials' }
+    it { expect( described_class.build_access_token_parms ).to eq '/um/oauth2/token?scope=tdxticket&grant_type=client_credentials' }
   end
 
   describe 'module related variables have the expected values' do
