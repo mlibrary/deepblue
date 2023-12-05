02cfce528cde0395864da97628cbe7f0cc70f8be
Fritz Freiheit <fritx@umich.edu>
2023-12-05 15:22:33 -0500

    DEEPBLUE-285 - Send works to APTrust - 5

--- app/models/aptrust/status.rb
+++ app/models/aptrust/status.rb
@@ -5,7 +5,7 @@ class Aptrust::Status < ApplicationRecord
   mattr_accessor :aptrust_status_debug_verbose, default: false
 
   def self.for_id( noid: )
-    Status.where( noid: noid )
+    where( noid: noid )
   end
 
 end
