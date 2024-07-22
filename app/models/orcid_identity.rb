a591243319f6d608c512d847c62a0fecef6ab9b9
Fritz Freiheit <fritx@umich.edu>
2024-07-22 13:02:31 -0400

    DEEPBLUE-353 - Fix ORCID identity model error

--- app/models/orcid_identity.rb
+++ app/models/orcid_identity.rb
@@ -3,7 +3,7 @@
 
 class OrcidIdentity < ApplicationRecord
 
-  serialize :profile_sync_preference, JSON if Rails.env.production?
+  # serialize :profile_sync_preference, JSON if Rails.env.production? # dev DB defines serialization natively, and this doesn't seem to work
 
   enum work_sync_preference: { sync_all: 0, sync_notify: 1, manual: 2 }
 
@@ -17,12 +17,24 @@ class OrcidIdentity < ApplicationRecord
 
   # Ensure we have an empty hash as a default value
   after_initialize do
-    self.profile_sync_preference ||= {}
+    if Rails.env.production?
+      self.profile_sync_preference ||= '{}'
+    else
+      self.profile_sync_preference ||= {}
+    end
   end
 
   def self.profile_sync_preference
     %i[education employment funding peer_reviews works].freeze
   end
+
+  # def profile_sync_preference=(value)
+  #   if Rails.env.production?
+  #     self.profile_sync_preference = do_serialize(value)
+  #   else
+  #     self.profile_sync_preference = value
+  #   end
+  # end
 
   def selected_sync_preferences
     psf = profile_sync_preference
@@ -31,7 +43,8 @@ class OrcidIdentity < ApplicationRecord
     elsif ( psf.is_a? String )
       psf = JSON.parse psf
     end
-    psf.select { |_k, v| v == "1" }.keys
+    rv = psf.select { |_k, v| v == "1" }.keys
+    return rv
   end
 
   protected
@@ -39,4 +52,28 @@ class OrcidIdentity < ApplicationRecord
     def set_user_orcid_id
       user.update(orcid: orcid_id)
     end
+
+    def do_deserialize( value )
+      if value.blank?
+        {}
+      elsif value.is_a? Hash
+        value
+      else
+        deserialized_state = ActiveSupport::JSON.decode value
+        return deserialized_state
+      end
+    rescue ActiveSupport::JSON.parse_error # rubocop:disable Lint/HandleExceptions
+      {}
+    end
+
+    def do_serialize( value )
+      if value.blank? || value.is_a?( String )
+        value
+      else
+        ActiveSupport::JSON.encode( value ).to_s
+      end
+    rescue ActiveSupport::JSON.parse_error # rubocop:disable Lint/HandleExceptions
+      {}
+    end
+
 end
