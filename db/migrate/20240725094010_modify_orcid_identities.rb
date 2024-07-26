f1b3bbff26d871ff588053770c0fbea5a3357d03
Fritz Freiheit <fritx@umich.edu>
2024-07-26 08:54:54 -0400

    Update 20240725094010_modify_orcid_identities.rb

--- db/migrate/20240725094010_modify_orcid_identities.rb
+++ db/migrate/20240725094010_modify_orcid_identities.rb
@@ -2,6 +2,8 @@
 
 class ModifyOrcidIdentities < ActiveRecord::Migration[5.2]
   def change
+    return unless table_exists?(:orcid_identities)
+    return if column_exists?(:orcid_identities, :profile_sync_preference, :text)
     remove_column :orcid_identities, :profile_sync_preference
     add_column :orcid_identities, :profile_sync_preference, :text
   rescue Exception => ignore
