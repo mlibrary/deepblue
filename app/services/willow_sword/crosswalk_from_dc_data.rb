1540de0469f1590b39b91b0937f365bfefaba13d
Fritz Freiheit <fritx@umich.edu>
2024-12-18 16:22:04 -0500

    DEEPBLUE-389 - Upgrade to Hyrax v5 - 4

--- app/services/willow_sword/crosswalk_from_dc_data.rb
+++ app/services/willow_sword/crosswalk_from_dc_data.rb
@@ -1,75 +1,75 @@
-module WillowSword
-  class CrosswalkFromDcData
-    attr_reader :dc, :metadata, :model, :mapped_metadata, :files_metadata, :terms, :translated_terms, :singular
-    def initialize(src_file, headers)
-      @src_file = src_file
-      @headers = headers
-      @dc = nil
-      @model = nil
-      @metadata = {}
-      @mapped_metadata = {}
-      @files_metadata = []
-    end
-
-    def terms
-      %w(subject_discipline rights_license authoremail methodology abstract 
-        accessRights accrualMethod accrualPeriodicity
-        accrualPolicy alternative audience available bibliographicCitation
-        conformsTo contributor coverage created creator date dateAccepted
-        dateCopyrighted dateSubmitted description educationLevel extent
-        format hasFormat hasPart hasVersion identifier instructionalMethod
-        isFormatOf isPartOf isReferencedBy isReplacedBy isRequiredBy issued
-        isVersionOf language license mediator medium modified provenance
-        publisher references relation replaces requires rights rightsHolder
-        source spatial subject tableOfContents temporal title type valid)
-    end
-
-    def translated_terms
-      {
-        'created' =>'date_created',
-        'rights' => 'rights_statement',
-        'relation' => 'related_url',
-        'type' => 'resource_type'
-      }
-    end
-
-    def singular
-      %w(rights authoremail rights_license)
-    end
-
-    def map_xml
-      parse_dc
-      @mapped_metadata = @metadata
-      assign_model if @metadata.any?
-    end
-
-    def parse_dc
-      return @metadata unless @src_file.present?
-      return @metadata unless File.exist? @src_file
-      f = File.open(@src_file)
-      doc = Nokogiri::XML(f)
-      # doc = Nokogiri::XML(@xml_metadata)
-      doc.remove_namespaces!
-      terms.each do |term|
-        values = []
-        doc.xpath("//#{term}").each do |t|
-          values << t.text if t.text.present?
-        end
-        key = translated_terms.include?(term) ? translated_terms[term] : term
-        values = values.first if values.present? && singular.include?(term)
-        @metadata[key.to_sym] = values unless values.blank?
-      end
-      f.close
-    end
-
-    def assign_model
-      unless @metadata.fetch(:resource_type, nil).blank?
-        @model = Array(@metadata[:resource_type]).map {
-          |t| t.underscore.gsub('_', ' ').gsub('-', ' ').downcase
-        }.first
-      end
-    end
-
-  end
-end
-
+# module WillowSword
+#   class CrosswalkFromDcData
+#     attr_reader :dc, :metadata, :model, :mapped_metadata, :files_metadata, :terms, :translated_terms, :singular
+#     def initialize(src_file, headers)
+#       @src_file = src_file
+#       @headers = headers
+#       @dc = nil
+#       @model = nil
+#       @metadata = {}
+#       @mapped_metadata = {}
+#       @files_metadata = []
+#     end
+#
+#     def terms
+#       %w(subject_discipline rights_license authoremail methodology abstract
+#         accessRights accrualMethod accrualPeriodicity
+#         accrualPolicy alternative audience available bibliographicCitation
+#         conformsTo contributor coverage created creator date dateAccepted
+#         dateCopyrighted dateSubmitted description educationLevel extent
+#         format hasFormat hasPart hasVersion identifier instructionalMethod
+#         isFormatOf isPartOf isReferencedBy isReplacedBy isRequiredBy issued
+#         isVersionOf language license mediator medium modified provenance
+#         publisher references relation replaces requires rights rightsHolder
+#         source spatial subject tableOfContents temporal title type valid)
+#     end
+#
+#     def translated_terms
+#       {
+#         'created' =>'date_created',
+#         'rights' => 'rights_statement',
+#         'relation' => 'related_url',
+#         'type' => 'resource_type'
+#       }
+#     end
+#
+#     def singular
+#       %w(rights authoremail rights_license)
+#     end
+#
+#     def map_xml
+#       parse_dc
+#       @mapped_metadata = @metadata
+#       assign_model if @metadata.any?
+#     end
+#
+#     def parse_dc
+#       return @metadata unless @src_file.present?
+#       return @metadata unless File.exist? @src_file
+#       f = File.open(@src_file)
+#       doc = Nokogiri::XML(f)
+#       # doc = Nokogiri::XML(@xml_metadata)
+#       doc.remove_namespaces!
+#       terms.each do |term|
+#         values = []
+#         doc.xpath("//#{term}").each do |t|
+#           values << t.text if t.text.present?
+#         end
+#         key = translated_terms.include?(term) ? translated_terms[term] : term
+#         values = values.first if values.present? && singular.include?(term)
+#         @metadata[key.to_sym] = values unless values.blank?
+#       end
+#       f.close
+#     end
+#
+#     def assign_model
+#       unless @metadata.fetch(:resource_type, nil).blank?
+#         @model = Array(@metadata[:resource_type]).map {
+#           |t| t.underscore.gsub('_', ' ').gsub('-', ' ').downcase
+#         }.first
+#       end
+#     end
+#
+#   end
+# end
+#
--- spec/services/willow_sword/integration_service_spec.rb
+++ spec/services/willow_sword/integration_service_spec.rb
@@ -1,50 +1,50 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-require_relative '../../../app/services/willow_sword/integration_service'
-
-RSpec.describe ::WillowSword::IntegrationService, skip: ::WillowSword::IntegrationService.skip_specs do
-
-  describe 'module debug verbose variables' do
-    let(:debug_verbose) { false }
-    it { expect( described_class.willow_sword_integration_service_debug_verbose ).to eq debug_verbose }
-  end
-
-  describe 'class variables' do
-    it { expect( described_class.default_collection_title ).to eq 'SWORDDefaultCollection' }
-  end
-
-  describe '#default_collection_id' do
-
-    context 'default_collection_id_cache is nil' do
-      let(:title) { described_class.default_collection_title }
-      let(:collection) { create(:collection_lw) }
-      let(:solr_query) { "+generic_type_sim:Collection AND +title_tesim:#{title}" }
-
-      before do
-        expect(::Hyrax::SolrService).to receive(:query).with(solr_query, rows: 10).and_return [collection]
-      end
-
-      it 'performs a solr query' do
-        described_class.default_collection_id_cache = nil
-        expect( described_class.default_collection_id ).to eq collection.id
-      end
-    end
-
-    context 'default_collection_id_cache is not nil' do
-      let(:cached_id) { 'cached id' }
-
-      before do
-        expect(::Hyrax::SolrService).to_not receive(:query)
-      end
-
-      it 'performs a solr query' do
-        described_class.default_collection_id_cache = cached_id
-        expect( described_class.default_collection_id ).to eq cached_id
-      end
-    end
-
-  end
-
-end
+# # frozen_string_literal: true
+#
+# require 'rails_helper'
+#
+# require_relative '../../../app/services/willow_sword/integration_service'
+#
+# RSpec.describe ::WillowSword::IntegrationService, skip: ::WillowSword::IntegrationService.skip_specs do
+#
+#   describe 'module debug verbose variables' do
+#     let(:debug_verbose) { false }
+#     it { expect( described_class.willow_sword_integration_service_debug_verbose ).to eq debug_verbose }
+#   end
+#
+#   describe 'class variables' do
+#     it { expect( described_class.default_collection_title ).to eq 'SWORDDefaultCollection' }
+#   end
+#
+#   describe '#default_collection_id' do
+#
+#     context 'default_collection_id_cache is nil' do
+#       let(:title) { described_class.default_collection_title }
+#       let(:collection) { create(:collection_lw) }
+#       let(:solr_query) { "+generic_type_sim:Collection AND +title_tesim:#{title}" }
+#
+#       before do
+#         expect(::Hyrax::SolrService).to receive(:query).with(solr_query, rows: 10).and_return [collection]
+#       end
+#
+#       it 'performs a solr query' do
+#         described_class.default_collection_id_cache = nil
+#         expect( described_class.default_collection_id ).to eq collection.id
+#       end
+#     end
+#
+#     context 'default_collection_id_cache is not nil' do
+#       let(:cached_id) { 'cached id' }
+#
+#       before do
+#         expect(::Hyrax::SolrService).to_not receive(:query)
+#       end
+#
+#       it 'performs a solr query' do
+#         described_class.default_collection_id_cache = cached_id
+#         expect( described_class.default_collection_id ).to eq cached_id
+#       end
+#     end
+#
+#   end
+#
+# end
