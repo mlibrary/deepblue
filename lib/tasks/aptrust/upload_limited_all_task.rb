96fdccf722cec245eea92bd6e62b5cb3497b9b1a
Fritz Freiheit <fritx@umich.edu>
2025-04-01 15:29:39 -0400

    DEEPBLUE-407 - Add clean ahoy visits job

--- lib/tasks/aptrust/upload_limited_all_task.rb
+++ lib/tasks/aptrust/upload_limited_all_task.rb
@@ -6,6 +6,26 @@ require_relative './upload_limited_task'
 
 module Aptrust
 
+SCHEDULER_ENTRY = <<-END_OF_SCHEDULER_ENTRY
+
+aptrust_upload:
+  # Run Monday and Wednesday starting at 9 AM (which is offset by 4 or [5 during daylight savings time], due to GMT)
+  # Note: all parameters are set in the rake task job
+  #      M H  D Mo WDay
+  cron: '0 14 * *  1,3'
+  class: RakeTaskJob
+  queue: scheduler
+  description: Run rake task aptrust:upload_limited_all
+  args:
+    by_request_only: false
+    hostnames:
+       - 'deepblue.lib.umich.edu'
+    job_delay: 0
+    subscription_service_id: aptrust_upload
+    rake_task: "aptrust:upload_limited_all"
+
+END_OF_SCHEDULER_ENTRY
+
   class UploadLimitedAllTask < ::Aptrust::AbstractUploadTask
 
     mattr_accessor :upload_limited_all_task_debug_verbose, default: true
